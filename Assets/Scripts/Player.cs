80a6cfde48d301062447916bb4b21771477a4153
Kudoshi <brendenguan@live.com>
2024-06-03 18:03:49 +0800

    feat: trap + offworld collider

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private int m_Gem;
+    private Vector2 m_SpawnPos;
+    private void Start()
+    {
+        m_SpawnPos = transform.position;
+    }
 
     public void AddGem(int gem)
     {
         m_Gem += gem;
         Debug.Log("Gem Count: " + m_Gem);
+    }
+
+    public void KillPlayer()
+    {
+        transform.position = m_SpawnPos;
     }
 }
